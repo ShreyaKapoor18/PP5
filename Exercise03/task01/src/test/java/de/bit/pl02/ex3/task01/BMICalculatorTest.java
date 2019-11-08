7152d12b6248412d384ee1cebba77c8b39a65e00
Shreya Kapoor <s0shkapo@uni-bonn.de>
2019-11-08 21:48:41 +0100

    change

--- Exercise03/task01/src/test/java/de/bit/pl02/ex3/task01/BMICalculatorTest.java
+++ Exercise03/task01/src/test/java/de/bit/pl02/ex3/task01/BMICalculatorTest.java
@@ -1,11 +1,34 @@
 package de.bit.pl02.ex3.task01;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import java.text.DecimalFormat;
 
-public class BMICalculatorTest extends TestCase {
+import org.junit.Test;
 
-	public void testBMI() {
-		fail("Not yet implemented");
+public class BMICalculatorTest3 {
+
+
+
+	
+	@Test
+	public void test() {
+		float height = (float) 1.87; 
+		float mass = 80;  
+		double bmi_ex = 22.87741; 
+		Person p2 = new Person(); 
+			// TODO Auto-generated method stub
+		p2.setHeight(height); 
+		p2.setMass(mass);
+		double bm = BMICalculator.BMI(p2); 
+		DecimalFormat dec = new DecimalFormat("#0.00000");
+		System.out.println(dec.format(bm));
+		double a = Double.parseDouble(dec.format(bm));
+		assertTrue(a== bmi_ex); 
+		
 	}
 
+
+
+
+	
 }
