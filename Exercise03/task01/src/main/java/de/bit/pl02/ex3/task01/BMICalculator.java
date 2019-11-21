953999134b00b34adf55f07f5510baecb21b75d7
sophiakrix <s0sokrix@uni-bonn.de>
2019-11-21 14:25:40 +0100

    exercise03 done

--- Exercise03/task01/src/main/java/de/bit/pl02/ex3/task01/BMICalculator.java
+++ Exercise03/task01/src/main/java/de/bit/pl02/ex3/task01/BMICalculator.java
@@ -4,13 +4,34 @@ import de.bit.pl02.ex3.task02.*;
 
 public class BMICalculator extends InputPerson{
 	private static double bmi; 
+	public double height;
+	public double weight_begin;
+	public double weight_end;
 	
-	public static double BMI (InputPerson person1) {
+	
+	public static double BMI (Person person1) {
 		bmi = (person1.getWeight() / (person1.getHeight() * person1.getHeight()));
 		return bmi;
 	}
 	public static void main(String[] args)
-	{
+	{  	
+	        double height = Double.parseDouble(args[0]);
+	             
+	        double weight_begin = Double.parseDouble(args[1]);
+	        
+	        double weight_end = Double.parseDouble(args[2]);
+	        
+	        for (double i= weight_begin;i<=weight_end; i+=0.5) {
+	        	
+	        	
+	        	Person p = new Person();
+	        	p.setHeight(height);
+	        	p.setWeight(i);
+	        	System.out.println(BMI(p));
+	        }
+	        	
+	        
+	       
 		
 	}
 
---
+++ Exercise03/task01/src/main/java/de/bit/pl02/ex3/task03/PrintBMI.java
@@ -0,0 +1,16 @@
+package de.bit.pl02.ex3.task03;
+
+import java.util.stream.IntStream;
+
+public class PrintBMI extends CommandLineInterface {
+
+	public static void main(String[] args) {
+		CommandLineInterface cli = new CommandLineInterface();
+		double[] range_weight = IntStream.rangeClosed(weight_begin, weight_end).toArray();
+		
+		
+		
+
+	}
+
+}
