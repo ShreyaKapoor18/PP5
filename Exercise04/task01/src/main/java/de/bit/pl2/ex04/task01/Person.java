c96f7241fbda43a11306f233feacadfbcc5eb373
sophiakrix <s0sokrix@uni-bonn.de>
2019-12-06 13:03:28 +0100

    exercise04 done

---
+++ Exercise04/task01/src/main/java/de/bit/pl2/ex04/task01/Person.java
@@ -0,0 +1,48 @@
+package de.bit.pl2.ex04.task01;
+
+import java.util.*;
+
+public class Person {
+
+	private String lastName;
+	private String firstName;
+	 
+	public Person() {
+		// Empty constructor 
+	}
+	
+	// setter methods 
+	public void setlastName(String lastName) { this.lastName = lastName; }
+	public void setfirstName(String firstName) { this.firstName = firstName; }
+	
+	// getter methods 
+	public String getlastName() { return lastName; }
+	public String getfirstName() { return firstName; }
+
+	
+	public static void main(String[] args)
+    { 
+		// array-based list data structure that can handle Person and Studen objects
+		//ArrayList<Person> personList = new ArrayList<Person>();
+		// sample data 
+		// size of input
+		
+		//String firstName[] = {"Sophia", "Shreya", "Gemma"};
+		//String lastName[] = {"Krix", "Kapoor", "van der Voort"};
+		//int n = firstName.length;
+		// instantiate objects of type Person and add them to the list
+		//int i; 
+		//for (i = 0; i < n; i++)
+		//{
+		//	Person person = new Person();
+		//	person.setfirstlName(firstName[i]);
+		//	person.setlastName(lastName[i]);
+		//	personList.add(person);
+		//}
+
+    
+    }
+}
+	
+	
+	
