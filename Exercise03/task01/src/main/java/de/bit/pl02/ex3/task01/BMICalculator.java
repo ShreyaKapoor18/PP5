09ad6dc8133928d23d4bd7a13a8f0c7278abdfa8
sophiakrix <s0sokrix@uni-bonn.de>
2019-12-20 11:52:38 +0100

    changes in exercise 04

--- Exercise03/task01/src/main/java/de/bit/pl02/ex3/task01/BMICalculator.java
+++ Exercise03/task01/src/main/java/de/bit/pl02/ex3/task01/BMICalculator.java
@@ -1,13 +1,14 @@
 package de.bit.pl02.ex3.task01;
 
-import de.bit.pl02.ex3.task02.*;
+// import de.bit.pl02.ex3.task02.*;
 
-public class BMICalculator extends InputPerson{
+public class BMICalculator {
 	private static double bmi; 
 	public double height;
 	public double weight_begin;
 	public double weight_end;
 	
+	Person person1 = new Person();
 	
 	public static double BMI (Person person1) {
 		bmi = (person1.getWeight() / (person1.getHeight() * person1.getHeight()));
@@ -21,15 +22,13 @@ public class BMICalculator extends InputPerson{
 	        
 	        double weight_end = Double.parseDouble(args[2]);
 	        
-	        for (double i= weight_begin;i<=weight_end; i+=0.5) {
-	        	
+	        for (double i= weight_begin;i<=weight_end; i+=0.5) {	        	
 	        	
 	        	Person p = new Person();
 	        	p.setHeight(height);
 	        	p.setWeight(i);
 	        	System.out.println(BMI(p));
 	        }
-	        	
 	        
 	       
 		
