ca4b34bfdeb973037553d63a418599a14a9fd149
Shreya Kapoor <s0shkapo@uni-bonn.de>
2019-12-27 22:52:41 +0100

    change

--- ProgrammingProject05/Task02/task02/src/main/java/de/bit/pl02/pp5/task02/Database.java
+++ ProgrammingProject05/Task02/task02/src/main/java/de/bit/pl02/pp5/task02/Database.java
@@ -1,103 +1,141 @@
-package de.bit.pl02.pp5.task02;
-
+package de.bit.pl02.pp5.task02new;
+import de.bit.pl02.pp5.task02new.Image; 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Database {
-	public static void main(String[] args) throws SQLException, IOException
-	{ 
+	private String name;
+	private int error_code; // giving an error when something goes wrong with the database. 
+	private Connection con; 
+	Database(String name)
+	{ // this constructor method asks the user for the name of the database.
+		this.name = name;
+		try {
+			this.con = this.Connect_db();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	public Connection Connect_db() throws SQLException
+	{ // connecting to the database whether existing or not existing!
 		Connection con = null; 
 		try {
 			Class.forName("org.sqlite.JDBC"); 
-			con = DriverManager.getConnection("jdbc:sqlite:test.db"); 
-			try {
-				Statement smt = con.createStatement(); 
-				String presence = "SELECT count(name) FROM sqlite_master WHERE type='TABLE' AND name='IMAGES'"; 
-				smt.execute(presence);				
-			// look for a method to put metadata in these columns using the metadata file. 
-			   
-			   ResultSet rs = smt.executeQuery("SELECT * from IMAGES"); 
-			   	while (rs.next())
-			   	{ 
-				   String x = rs.getString("ID"); 
-				   String s = rs.getString("TITLE");
-				   System.out.println(x+" "+s);   
-			   	}
-			}catch(Exception e) {
-				Statement smt = con.createStatement(); 
-				String sql = "CREATE TABLE IMAGES " +
-						       "(ID TEXT PRIMARY KEY NOT NULL,"+ 
-							"TITLE   TEXT NOT NULL, AUTHOR TEXT NOT NULL)";
-				smt.execute(sql); 
-				 String sql1 = "ALTER TABLE IMAGES ADD COLUMN PICTURE blob"; 
-				 smt.execute(sql1); 
-				 System.out.println(sql1); 
-				 readmetadata("//Users//shreyakapoor//Desktop//PP5"); 
-				e.printStackTrace();
-			}
+			con = DriverManager.getConnection("jdbc:sqlite:" + this.name + ".db"); 
+			return con; 
+			
 		} catch (ClassNotFoundException e) { 
 			e.printStackTrace();
+			return null; 
 		}
+	}
+	public void make_table()
+	{ 
+		try {
+			Statement stmt = this.con.createStatement(); // try to query with the help of certain statements.
+			ArrayList<String> sqls = this.insert_fields(); 
+			for (String sql: sqls) { 
+				try { 
+				// See if these are getting added or not.
+				System.out.println("create tables!"); 
+				stmt.execute(sql); } 
+				catch (SQLException e) {
+					continue; // if there is an erro means it does contain the columns etc.
+				}
+			stmt.close(); 
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println(e.getMessage()); 
+		}
+	}
+	public ArrayList<String> insert_fields()
+	{ 	System.out.println("adding the fields"); 
+		// arraylist of SQL commands which can be given to the program so that the execution gets up and running. 
+		ArrayList<String> commands = new ArrayList<String>();
+		commands.add("CREATE TABLE IF NOT EXISTS IMAGES " +"(ID TEXT PRIMARY KEY NOT NULL,"+ "TITLE   TEXT NOT NULL, AUTHOR TEXT NOT NULL)"); 
+		commands.add("ALTER TABLE IMAGES PICTURE ADD COLUMN PICTURE blob"); // add a column so that pictures can be stored there.
+		
+		return commands; 
 		
 	}
-	public static void readmetadata(String dir) throws IOException, SQLException, ClassNotFoundException
-	{ 	Class.forName("org.sqlite.JDBC"); 
-		Connection con = DriverManager.getConnection("jdbc:sqlite:test.db");
-		Statement smt = con.createStatement();
-		smt.execute("SELECT * FROM IMAGES"); 
+	public void see_table() throws SQLException
+	{ 	System.out.println("printing"); 
+		Statement smt = this.con.createStatement(); 
+		ResultSet rs = smt.executeQuery("SELECT * from IMAGES"); 
+	   	while (rs.next())
+	   	{  System.out.println("result set!"); 
+		   String x = rs.getString("ID"); 
+		   String s = rs.getString("TITLE");
+		   String a = rs.getString("AUTHOR"); 
+		   System.out.println(x+" "+s);   
+	   	}
+	   	ResultSet rs2 = smt.executeQuery("PRAGMA table_info('IMAGES')"); 
+	   	while (rs2.next())
+	   	{  //System.out.println("result set2!");    
+	   	}
+	   	smt.close(); 
+	   
+	}
+	public ArrayList<String> read_director(String dir) throws SQLException
+	{  
+		Statement smt = this.con.createStatement(); 
 		File dir1 = new File(dir); 
     	File[] filesindir = dir1.listFiles();
-    	if (filesindir!= null) { 
-    		for (File child: filesindir )
-    		{ 	String filename = child.getName(); 
-    			if (filename.contains(".meta")){
-    				System.out.println(filename); 
-    				FileReader filer = new FileReader(child.getAbsolutePath());
-    				BufferedReader buffr = new BufferedReader(filer);
-    				boolean eof = false;
-    				String author = "xx"; 
-    				String title = "yy"; 
-    				String Id; 
-    				while ((!eof))
-    				  {
-    				  String s = buffr.readLine();
-    				  if(s == null){
-    				    eof = true;
-    				       }
-    				  else{
-    				    System.out.println(s);
-    				    if (s.contains("Title:")){
-    				    	title = s.split(":")[1]; 
-    				    }
-    				    else if (s.contains("Author:")) { 
-    				    	author = s.split(":")[1]; 
-    				    }
-    				   
-    				  	}
-    				}
-    				buffr.close();
-    				if ( author.length()>=2 && title.length()>=5){
-    					Id = author.substring(0, 2)+title.substring(0,4)+filename.split("\\.")[0];
-    				} 
-    				else {
-    					Id = filename.split("-")[0]; 
-    				}
- 
-    				String sql = "INSERT INTO IMAGES (ID,TITLE, AUTHOR)" + 
-    							"VALUES ("+ Id+ ","+ title + "," + author +")";
+    	ArrayList<String> arr = new ArrayList<String>();
+    	for (File f: filesindir)
+    	{ 	String imgname = f.getName();
+    		if (imgname.contains(".png")|| imgname.contains(".jpg") || imgname.contains(".jpeg")){ 
+    			Image img = new Image(f.getAbsolutePath(), imgname); 
+    			ArrayList<String> meta = img.find_metadata(f.getAbsolutePath()); 
+    			arr.add("INSERT INTO IMAGES (ID,TITLE, AUTHOR)" + "VALUES ('"+ meta.get(0)+ "','"+ meta.get(1) + "'," + meta.get(2) +"')"); 
+    			try {
+    				String id = "'"+ meta.get(0)+ "'"; 
+    				String author = "'"+ meta.get(2)+ "'"; 
+    				String title = "'"+ meta.get(1)+ "'"; 
+    				System.out.println("inserting the image metadata here!" + id + title + author ); 
+    		
+    				String sql = "INSERT INTO IMAGES (ID,TITLE, AUTHOR) VALUES ("+ id + "," + title +  "," + author + ")";
+    				smt.execute(sql);
     				System.out.println(sql); 
-    				smt.executeUpdate(sql); 
-    				
-    				
-    			} 
-    		} 
+    			}catch (Exception e) {
+    				System.out.println(e.getMessage()); 
+    				//continue; // only this particular image could not be inserted into the database.
+    			}
+    			}
     	}
+    	System.out.println("inserting images");
+
+    	return arr; 
 	}
-	
-	
+	public static void main(String[] args) throws SQLException
+	{ 
+		Scanner input = new Scanner(System.in); 
+		System.out.println("Enter the name of the database you want to make/see"); 
+		String name = input.nextLine(); 
+		Database Db = new Database(name); 
+		Db.make_table(); 
+		//get the path of the directory where you want to insert all the images from!
+		Scanner input2 = new Scanner(System.in); 
+		System.out.println("Enter the file directory from which you want the images"); // also need to connect this to restful API
+		String dir = input2.nextLine(); 
+		input2.close();
+		Db.read_director(dir); 
+		try {
+			Db.see_table(); // cam be done with the help of command line parameters, whether these options shall be present or not. 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
--- ProgrammingProject05/Task02/task02/src/main/java/de/bit/pl02/pp5/task02/Image.java
+++ ProgrammingProject05/Task02/task02/src/main/java/de/bit/pl02/pp5/task02/Image.java
@@ -1,70 +1,92 @@
-package de.bit.pl02.pp5.task02;
+package de.bit.pl02.pp5.task02new;
 
-import java.io.*;
-import java.sql.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Image {
-	   private byte[] readFile(String file) {
-	        ByteArrayOutputStream bos = null;
-	        try {
-	            File f = new File(file);
-	            FileInputStream fis = new FileInputStream(f);
-	            byte[] buffer = new byte[1024];
-	            bos = new ByteArrayOutputStream();
-	            for (int len; (len = fis.read(buffer)) != -1;) {
-	                bos.write(buffer, 0, len);
-	            }
-	        } catch (FileNotFoundException e) {
-	            System.err.println(e.getMessage());
-	        } catch (IOException e2) {
-	            System.err.println(e2.getMessage());
-	        }
-	        return bos != null ? bos.toByteArray() : null;
-	    }
-	   private Connection connect() {
-	        // SQLite connection string
-	        String url = "jdbc:sqlite:images2.db";
-	        Connection conn = null;
-	        try {
-	            conn = DriverManager.getConnection(url);
-	        } catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	        }
-	        return conn;
-	    }
-	    public void updatePicture(String Id, String filename) {
-	        // update sql
-	        String updateSQL = "UPDATE IMAGES " + "SET PICTURE =?"
-	                + "WHERE id=?";
-	 
-	        try  {
-	        	Connection conn = connect();
-                PreparedStatement pstmt = conn.prepareStatement(updateSQL); 
-	            // set parameters
-	            pstmt.setBytes(1, readFile(filename));
-	            pstmt.setString(2, Id);
-	 
-	            pstmt.executeUpdate();
-	            System.out.println("Stored the file in the BLOB column.");
-	 
-	        } catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	        }
-	    }
-	    public static void main(String[] args) {
-	    	File dir = new File("//Users//shreyakapoor//Desktop//PP5"); 
-	    	File[] filesindir = dir.listFiles();
-	    	if (filesindir!= null) { 
-	    		for (File child: filesindir )
-	    		{ 	String filename = child.getName(); 
-	    			if (filename.contains("jpg") || filename.contains("png") || filename.contains("jpeg")){
-	    				Image app = new Image();
-	    				// how to put the id of the images!
-	    				app.updatePicture(filename, child.getAbsolutePath());
-	    			} 
-	    		} 
-	    	} 
-	    }
-
+	private byte[] blob; // so that this image can be entered into the blob columb 
+	private String name; 
+	private String path; 
+	Image(String path, String name)
+	{
+		this.blob = readFile(path); 
+		this.name = name; 
+		this.path = path; 
+	}
+	private byte[] readFile(String file) {
+        ByteArrayOutputStream bos = null;
+        try {
+            File f = new File(file);
+            FileInputStream fis = new FileInputStream(f);
+            byte[] buffer = new byte[1024];
+            bos = new ByteArrayOutputStream();
+            for (int len; (len = fis.read(buffer)) != -1;) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e2) {
+            System.err.println(e2.getMessage());
+        }
+        return bos != null ? bos.toByteArray() : null;
+    }
+	ArrayList<String> find_metadata(String path)
+	{
+		/* we consider that we follow the same path for metadata files and the actual image files 
+		 so we can just strip off the . in the filename and then add .meta infront of it in order to get the
+		 related to a particular image. 
+		 */
+		ArrayList<String> meta = new ArrayList<String>(); 
+		String metapath = this.path.split("\\.")[0]+ ".meta";
+		try { 
+			FileReader filer = new FileReader(metapath);
+			BufferedReader buffr = new BufferedReader(filer);
+			boolean eof = false;
+			String author = "xx"; 
+			String title = "yyzz"; 
+			String Id; 
+			while ((!eof))
+			{
+			  String s = buffr.readLine();
+			  if(s == null){
+			    eof = true;
+			       }
+			  else{
+			    System.out.println(s);
+			    if (s.contains("Title:")){
+			    	title = s.split(":")[1]; }
+			    else if (s.contains("Author:")) { 
+			    	author = s.split(":")[1]; }
+			   }
+			}
+			if ( author.length()>=2 && title.length()>=5){
+				Id = author.substring(0, 2)+title.substring(0,4)+this.name.split("\\.(?=[^\\.]+$)")[0];
+			} 
+			else {
+				Id = this.name.split("\\.(?=[^\\.]+$)")[0]; // dot followed by a number of non fos
+			}
+			meta.add(Id);
+			meta.add(title); 
+			meta.add(author); 
+		
+		}catch (Exception e) {
+			/* if there existed none of such files then probably some 
+			 default values shall be set and passed on in the given fields.
+			 */
+			meta.add(this.name.split("\\.(?=[^\\.]+$)")[0]); 
+			meta.add("xx"); 
+			meta.add("yyzz"); 
+		}
+		return meta;
+		
+		
+	}
+	
 
 }
